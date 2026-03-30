f8bf932a8545e69ef366ce5ecfb07edaba12ae80
DevAniela <dismal.damsel@gmail.com>
2026-03-30 14:29:56 +0300

    Initial Spring Boot setup

---
+++ src/main/java/com/anavi/actingcoach/ActingcoachApplication.java
@@ -0,0 +1,13 @@
+package com.anavi.actingcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActingcoachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActingcoachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anavi/actingcoach/ActingcoachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anavi.actingcoach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActingcoachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
